9e0bd8477680b10d46a5ac9f3793b4e2b2a09774
myprojectswork <vinitaweconnect@gmail.com>
2026-02-11 14:39:49 +0530

    Delete games directory

--- games/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-// Universal game hub - DO NOT MODIFY
-window.GameHub = {
-    gamesLoaded: new Set(),
-    registerGame: function(gameName) {
-        this.gamesLoaded.add(gameName);
-        console.log(`✓ ${gameName} loaded`);
-    }
-};
--- games/includes/header.php
+++
@@ -1,6 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $pageTitle ?? 'Gaming Zone'; ?></title>
-    <link rel="stylesheet" href="assets/css/style.css?v=1">
-</head>
